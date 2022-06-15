140c81b55cd47702a7d62704418191e11495231c
neilshah2000 <neilshahlimited@hotmail.com>
2022-06-15 12:31:13 +0100

    delete comment

--- src/mds/types/mds.d.ts
+++ src/mds/types/mds.d.ts
@@ -2,7 +2,7 @@ declare namespace MDS {
     let rpchost: string;
     let pollhost: string;
     let logging: boolean;
-    function init(callback: (event:any) => void); // any type not NodeEvent
+    function init(callback: (event: any) => void);
     function log(output: string);
     function cmd(command: string, callback: (data: any) => void);
     interface form {getParams: (param: string) => void}
